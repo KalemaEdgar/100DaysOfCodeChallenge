4da199b07585abf8984e1120ef052709b258cafe
Kalema Edgar <eked290@gmail.com>
2021-03-29 00:11:56 +0300

    Day 28: Sending mail
    You need to configure JavaMail API and Java Activation Framework (JAF) and add add mail.jar and activation.jar files in your CLASSPATH for these to work

---
+++ src/com/learning/tests/Test.java
@@ -0,0 +1,15 @@
+package com.learning.tests;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Math.sqrt(-4));
+
+        int x = 0;
+        //int y = 4 % x;
+
+        System.out.println((!( 4 > 2 )));
+
+        byte b = 4; b++;
+        System.out.println(b);
+    }
+}
